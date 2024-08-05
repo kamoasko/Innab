6f2ef167b39a1c58f2623808cc4301df337525cd
kamoasko <kamdesigning@gmail.com>
2024-08-05 18:16:10 +0400

    scrollToTop component created and done

--- src/App.jsx
+++ src/App.jsx
@@ -20,46 +20,50 @@ import DetailPage from "./pages/DetailPage";
 import Projects from "./pages/Projects";
 import ContactPage from "./pages/ContactPage";
 import Privacy from "./pages/Privacy";
+import ScrollToTop from "./components/scrollToTop";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<MainLayout />}>
-        <Route index element={<Homepage />} />
-        <Route path="about">
-          <Route index element={<About />} />
-          <Route path="vacancies" element={<VacanciesPage />} />
-          <Route path="contact" element={<ContactPage />} />
-        </Route>
-
-        <Route path="trainings" element={<TrainingsPage />} />
-        <Route path="corporative" element={<Corporative />} />
-        <Route path="projects" element={<Projects />} />
-        <Route path="career-center" element={<CareerCenter />} />
-
-        <Route path="useful-for-you">
-          <Route path="video-lessons">
-            <Route index element={<VideoLessons />} />
-            <Route path=":id" element={<DetailPage />} />
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route index element={<Homepage />} />
+          <Route path="about">
+            <Route index element={<About />} />
+            <Route path="vacancies" element={<VacanciesPage />} />
+            <Route path="contact" element={<ContactPage />} />
           </Route>
-          <Route path="blog">
-            <Route index element={<BlogPage />} />
-            <Route path=":id" element={<DetailPage />} />
+
+          <Route path="trainings" element={<TrainingsPage />} />
+          <Route path="corporative" element={<Corporative />} />
+          <Route path="projects" element={<Projects />} />
+          <Route path="career-center" element={<CareerCenter />} />
+
+          <Route path="useful-for-you">
+            <Route path="video-lessons">
+              <Route index element={<VideoLessons />} />
+              <Route path=":id" element={<DetailPage />} />
+            </Route>
+            <Route path="blog">
+              <Route index element={<BlogPage />} />
+              <Route path=":id" element={<DetailPage />} />
+            </Route>
+            <Route path="career-calculator" element={<CareerCalculator />} />
+            <Route path="seminar-and-webinar" element={<SeminarWebinar />} />
+            <Route path="internships" element={<InternShips />} />
           </Route>
-          <Route path="career-calculator" element={<CareerCalculator />} />
-          <Route path="seminar-and-webinar" element={<SeminarWebinar />} />
-          <Route path="internships" element={<InternShips />} />
-        </Route>
 
-        <Route path="news">
-          <Route index element={<NewsPage />} />
-          <Route path=":id" element={<NewsDetail />} />
-        </Route>
+          <Route path="news">
+            <Route index element={<NewsPage />} />
+            <Route path=":id" element={<NewsDetail />} />
+          </Route>
 
-        <Route path="/privacy-policy" element={<Privacy />} />
-      </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+          <Route path="/privacy-policy" element={<Privacy />} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
