c312c39a38a1390d1bedb7128ad32d0ccdac5b7d
kamoasko <kamdesigning@gmail.com>
2024-09-28 17:02:47 +0400

    beginning from top when page change added

--- src/layouts/mainLayout/MainLayout.jsx
+++ src/layouts/mainLayout/MainLayout.jsx
@@ -1,11 +1,15 @@
-// src/components/Layout.js
-import React, { useRef } from "react";
-import { Outlet } from "react-router-dom";
+import React, { useEffect, useRef } from "react";
+import { Outlet, useLocation } from "react-router-dom";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
 
 const MainLayout = () => {
   const partnersRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0); // Scroll to top on route change
+  }, [location]);
 
   return (
     <>
