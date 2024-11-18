2a0f084094f3038d868c8f0c7a7f89a40ccd5375
kamoasko <kamdesigning@gmail.com>
2024-11-18 23:43:27 +0400

    target blank added to customers card link

--- src/components/CustomerCard/index.jsx
+++ src/components/CustomerCard/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const CustomerCard = ({ img, to }) => {
   return (
     <figure className={styles.customerCard}>
-      <Link to={to}>
+      <Link to={to} target="_blank">
         <picture>
           <img loading="lazy" src={img} alt="" />
         </picture>
